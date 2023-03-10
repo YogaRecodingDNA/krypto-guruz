ceecb3370f229b15537563de3a676ed396b2f9d8
Chadd Ho <74035551+YogaRecodingDNA@users.noreply.github.com>
2023-03-09 16:33:54 -0800

    Create React App | Download Packages | Configures

---
+++ hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomiclabs/hardhat-waffle");
+const fs = require('fs');
+
+// eslint-disable-next-line no-undef
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
